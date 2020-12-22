8c6459cae76e5efd87b40b262d9ed708758ddacd
David <davcoding1@gmail.com>
2020-12-22 12:17:23 -0600

    Added Tribute Page

---
+++ Main Portfolio/HTML-CSS Page/Tribute Page/FreeCodeCamp_JS_TributePage.js
@@ -0,0 +1,98 @@
+var play1 = $("#song1")[0];
+var play2 = $("#song2")[0]; 
+var play3 = $("#song3")[0]; 
+var play4 = $("#song4")[0]; 
+var play5 = $("#song5")[0]; 
+var play6 = $("#song6")[0]; 
+var play7 = $("#song7")[0]; 
+var play8 = $("#song8")[0]; 
+var play9 = $("#song9")[0]; 
+var play10 = $("#song10")[0]; 
+var play11 = $("#song11")[0]; 
+var play12 = $("#song12")[0]; 
+var play13 = $("#song13")[0]; 
+var play14 = $("#song14")[0]; 
+
+$("#circle-1").hover(function(){
+    play1.play();
+}, function(){
+    play1.pause(); 
+});
+
+$("#circle-2").hover(function(){
+  play2.play();
+}, function(){
+  play2.pause(); 
+});
+
+$("#circle-3").hover(function(){
+  play3.play();
+}, function(){
+  play3.pause(); 
+});
+
+$("#circle-4").hover(function(){
+  play4.play();
+}, function(){
+  play4.pause(); 
+});
+
+$("#circle-5").hover(function(){
+  play5.play();
+}, function(){
+  play5.pause(); 
+});
+
+$("#circle-6").hover(function(){
+  play6.play();
+}, function(){
+  play6.pause(); 
+});
+
+$("#circle-7").hover(function(){
+  play7.play();
+}, function(){
+  play7.pause(); 
+});
+
+$("#circle-8").hover(function(){
+  play8.play();
+}, function(){
+  play8.pause(); 
+});
+
+$("#circle-9").hover(function(){
+  play9.play();
+}, function(){
+  play9.pause(); 
+});
+
+$("#circle-10").hover(function(){
+  play10.play();
+}, function(){
+  play10.pause(); 
+});
+
+$("#circle-11").hover(function(){
+  play11.play();
+}, function(){
+  play11.pause(); 
+});
+
+$("#circle-12").hover(function(){
+  play12.play();
+}, function(){
+  play12.pause(); 
+});
+
+$("#circle-13").hover(function(){
+  play13.play();
+}, function(){
+  play13.pause(); 
+});
+
+$("#circle-14").hover(function(){
+  play14.play();
+}, function(){
+  play14.pause(); 
+});
